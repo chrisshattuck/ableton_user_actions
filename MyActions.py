7e9b719fc82a868207849321d1ea5792ba81c58f
Chris Shattuck <ashattuc@gmail.com>
2021-01-22 14:29:22 -0700

    Messed something up, about to roll back some.

--- MyActions.py
+++ MyActions.py
@@ -8,74 +8,118 @@ class MyActions(UserActionsBase):
         self.add_global_action('crossfade_random', self.crossfade_random)
         self.cf_vars = {}
         self.log_message = ''
+        self.initialized = []
+        self.call_count = 0
 
     # Converts arguments in the form of "key=value,key=value"
     # to a dictionary.
-    def prepare_args(self, args):
+    def prepare_args(self, args, default_args={}):
         args = args.split(",")
         new_args = {}
         for k, v in enumerate(args):
             arg_parts = v.split('=')
-            new_args[arg_parts[0]] = arg_parts[1].strip('"\'')
+            new_args[arg_parts[0]] = arg_parts[1].strip('"\'').strip()
+        for k, v in default_args.items():
+            if k not in new_args:
+                new_args[k] = v
         return new_args
 
+    # Keep ongoing log string.
     def log(self, message):
         self.log_message += '\n\n' + message + '\n\n'
 
+    # Output the log string.
     def output_log(self):
         self.canonical_parent.log_message(self.log_message)
+        self.log_message = ''
 
+    # Shortcut for triggering action.
     def run_action(self, action):
         self.canonical_parent.clyphx_pro_component.trigger_action_list(action)
 
+    def run_action_list(self, action_list):
+        action = self.prepare_action(action_list)
+        self.run_action(action)
+
+    # Takes list and concatenates it into an action list.
     def prepare_action(self, action_list):
         return ';'.join(action_list)
 
-    # TODO: If working between two tracks that aren't identical, this won't work. But it could.
+    # Randomly crossfades between two tracks.
+    #
+    # Uses the following arguments:
+    # - track="Name of track"
+    # - fadetime=100 (defaults to 100)
+    # Does the following:
+    # - Duplicates the track.
+    # - Sets the volume of original and duplicated track to 0.
+    # - Selects a random clip in the original track,
+    #    sets a random start time within the range of
+    #    the clip's loop.
+    # - Plays the clip.
+    # - Fades the clip in.
+    # - The next time the x-clip plays, it will select
+    #   a random cip from the duplicated track, fade it
+    #   in and fade out the original.
+    # - Subsequent plays repeat the random crossfade.
+    # For this to work right, ensure the following:
+    # - Clips should be set to loop.
+    # - Clips in the duplicated track should be
+    #   identical to the original.
+    # - To repeat this, set it on a looping clip and use
+    #   (LSEQ), as in:
+    #   [] (LSEQ) crossfade_random track="Name of track"
     def crossfade_random(self, action_def, args):
-
-        args = self.prepare_args(args)
+        default_args = {
+            'fadetime': 100
+        }
+        args = self.prepare_args(args, default_args)
         cf_id = args['track']
         tracklist = list(self.song().tracks)  # Type as list, won't work otherwise
 
-        # Initialize crossfade
-        if not cf_id in self.cf_vars:
+        self.call_count += 1
+
+        # Reset passed args
+        if cf_id in self.cf_vars:
+            for key, value in args.items():
+                self.cf_vars[cf_id][key] = value
+
+        # Run first time this runs on a track
+        else:
             self.cf_vars[cf_id] = args  # Process arguments from x-action
 
             # Duplicate track if not duplicated
-            dupe_track_name = self.cf_vars[cf_id]['track'] + ' (COPY)'
-            self.cf_vars[cf_id]['dupe'] = dupe_track_name
+            self.cf_vars[cf_id]['dupe'] = self.cf_vars[cf_id]['track'] + ' (COPY)'
             duplicate_exists = False
             for track in tracklist:
-                if track.name == dupe_track_name:
+                if track.name == self.cf_vars[cf_id]['dupe']:
                     duplicate_exists = True
-            rename_next = False
 
             if not duplicate_exists:
                 self.run_action('"' + self.cf_vars[cf_id]['track'] + '"/ DUPE')
+                # Regenerate track list including duplicate
                 tracklist = list(self.song().tracks)
                 rename_next = False
                 for i, track in enumerate(tracklist):
-
                     if rename_next:
                         rename_next = False
                         track_index = i + 1
-                        self.run_action(str(track_index) + '/ NAME "' + dupe_track_name + '"')
+                        self.run_action(str(track_index) + '/ NAME "' + self.cf_vars[cf_id]['dupe'] + '"')
                         break
                     if track.name == self.cf_vars[cf_id]['track']:
                         rename_next = True
-
-            self.cf_vars[cf_id]['playing_track_name'] = self.cf_vars[cf_id]['dupe']  # Set default playing
+            # Set default playing track
+            self.cf_vars[cf_id]['playing_track_name'] = self.cf_vars[cf_id]['dupe']
+            # Set volume on both tracks to 0
             action_list = [
+                'WAIT 5',
                 '"' + self.cf_vars[cf_id]['track'] + '"/ VOL 0',
                 '"' + self.cf_vars[cf_id]['dupe'] + '"/ VOL 0',
             ]
-            action = self.prepare_action(action_list)
-            self.run_action('WAIT 5; "' + self.cf_vars[cf_id]['track'] + '"/ VOL 0')
-            self.run_action('WAIT 5; "' + self.cf_vars[cf_id]['dupe'] + '"/ VOL 0')
-            self.log(action)
+            self.run_action_list(action_list)
         # End initialize
 
+        # Set the current and next tracks.
         current_track = self.cf_vars[cf_id]['track']
         next_track = self.cf_vars[cf_id]['dupe']
         if self.cf_vars[cf_id]['playing_track_name'] == self.cf_vars[cf_id]['dupe']:
@@ -111,7 +155,9 @@ class MyActions(UserActionsBase):
                     action_after_clip = ''
                     wait = 'WAIT 3; '
                 action_play_random = wait + action_before_clip + action_after_clip
-                self.log('PLAY RANDOM:' + action_play_random)
+
+        if not 'fadetime' in args:
+            self.cf_vars[cf_id]['fadetime'] = 100
 
         # Generate action for changing the loop start time and
         # performing crossfade.
@@ -119,14 +165,11 @@ class MyActions(UserActionsBase):
             'WAIT 2',
             '"' + next_track + '"/CLIP START RND song.view.detail_clip.loop_start-song.view.detail_clip.loop_end',
             'WAIT 4',
-            '"' + current_track + '"/VOL RAMP 100 0',
-            '"' + next_track + '" / VOL RAMP 100 100',
+            '"' + current_track + '"/VOL RAMP ' + str(self.cf_vars[cf_id]['fadetime']) + ' 0',
+            '"' + next_track + '" / VOL RAMP ' + str(self.cf_vars[cf_id]['fadetime']) + ' 100',
         ]
-        action = self.prepare_action(action_list)
-        self.run_action(action_play_random)
-        self.run_action(action)
+        self.run_action_list(action_list)
 
         self.cf_vars[cf_id]['playing_track_name'] = next_track
 
         self.output_log()
-
